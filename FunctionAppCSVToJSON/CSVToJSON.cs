bb5ee87b409b558eea21b5697dca2278ac99861e
Houston Brignano <hbrignano@aegworldwide.com>
2018-12-06 16:19:04 -0800

    Change it all to use csvHelper

--- FunctionAppCSVToJSON/CSVToJSON.cs
+++ FunctionAppCSVToJSON/CSVToJSON.cs
@@ -11,6 +11,8 @@ using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+using CsvHelper;
+using System.Text;
 
 namespace FunctionAppCSVToJSON
 {
@@ -21,30 +23,13 @@ public static class CSVToJSON
     {
         log.Info("C# HTTP trigger function CSVToJSON processed a request.");
 
-        char[] fieldSeperator = new char[] { ',' };
-
-        string fileName = req.Query["fileName"];            
-        string rowsToSkipStr = req.Query["rowsToSkip"];
+        string fileName = req.Query["fileName"];
         string errorMessage = "";
 
         string requestBody = new StreamReader(req.Body).ReadToEnd();
         dynamic data = JsonConvert.DeserializeObject(requestBody);
-        int rowsToSkip = 0;
-        long lineSkipCounter = 0;
 
         fileName = fileName ?? data?.fileName;          
-        rowsToSkipStr = rowsToSkipStr ?? data?.rowsToSkip;
-
-        if (rowsToSkipStr == null)
-        {
-            errorMessage = "Please pass a rowsToSkip on the query string or in the request body";
-            log.Info("BadRequest: " + errorMessage);
-            return new BadRequestObjectResult(errorMessage);
-        }
-        else
-        {
-            Int32.TryParse(rowsToSkipStr, out rowsToSkip);                
-        }
 
         if (fileName == null)
         {
@@ -64,75 +49,57 @@ public static class CSVToJSON
 
         log.Info("csv data is present.");
 
-        string[] csvLines = ToLines(csvData);
+        
 
-        log.Info(string.Format("There are {0} lines in the csv content {1}.", csvLines.Count(), fileName));
-
-        var headers = csvLines[0].Split(fieldSeperator).ToList<string>();
+        
 
         JsonResult resultSet = new JsonResult(fileName);
 
+        byte[] byteArray = Encoding.UTF8.GetBytes(csvData);
+        MemoryStream csvStream = new MemoryStream(byteArray);
 
-        foreach (var line in csvLines.Skip(rowsToSkip))
-        {
-            //Check to see if a line is blank.
-            //This can happen on the last row if improperly terminated.
-            if (line != "" || line.Trim().Length > 0 )
-            {
-                var lineObject = new JObject();
-                var fields = line.Split(fieldSeperator);
+        var records = Convert(csvStream);
 
-                for (int x = 0; x < headers.Count; x++)
-                {
-                    lineObject[headers[x]] = fields[x];
-                }
+        JArray jsonarray = JArray.FromObject(records);
 
-                resultSet.Rows.Add(lineObject);
-            }
-            else
-            {
-                lineSkipCounter += 1;
-            }
+        foreach(var row in jsonarray.Children()){
+            resultSet.Rows.Add(row);
         }
 
-        log.Info(string.Format("There were {0} lines skipped, not including the header row.", lineSkipCounter));
+        
+
+        log.Info(string.Format("There are {0} lines in the csv records content {1}.", records.Count(), fileName));
+
+        
+        
 
         return (ActionResult)new OkObjectResult(resultSet);
     }
 
-    private static string[] ToLines(string dataIn)
-        {            
-            char[] EOLMarkerR = new char[] { '\r' };
-            char[] EOLMarkerN = new char[] { '\n' };
-            char[] EOLMarker = EOLMarkerR;
+        public static List<object> Convert(Stream blob)
+        {
+            var sReader = new StreamReader(blob);
+            var csv = new CsvReader(sReader);
 
-            //check to see if the file has both \n and \r for end of line markers.
-            //common for files comming from Unix\Linux systems.
-            if (dataIn.IndexOf('\n') > 0 && dataIn.IndexOf('\r') > 0)
-            {
-                //if we find both just remove one of them.
-                dataIn = dataIn.Replace("\n", "");
-            }
-            //If the file only has \n then we will use that as the EOL marker to seperate the lines.
-            else if(dataIn.IndexOf('\n') > 0)
-            {
-                EOLMarker = EOLMarkerN; 
-            }
+            csv.Read();
+            csv.ReadHeader();
 
-            //How do we know the dynamic data will have Split capability?
-            return dataIn.Split(EOLMarker);
+            var csvRecords = csv.GetRecords<object>().ToList();
+
+            return (csvRecords);
         }
-    }
+        
 
     public class JsonResult
     {
         public JsonResult(string fileName)
         {
-            Rows = new List<object>();
+            Rows = new JArray();
             FileName = fileName;
         }
 
         public string FileName { get; set; }
-        public List<object> Rows { get; set; }
+        public JArray Rows { get; set; }
     }
+}
 }
